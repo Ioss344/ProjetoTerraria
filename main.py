4c44d234041beda6f6a24c8dba3ca13247cf4e67
unknown <carlineoss344@gmail.com>
2025-05-04 16:52:58 -0300

    adicao do projeto de codigo 1

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-Lalala lili
+import tkinter as tk
+
+#Criando a janela
+janela = tk.Tk()
+janela.geometry('1920x1080')
+janela.title('Projeto Terraria')
+
+#Texto da interface
+texto_janela = tk.Text(janela)
+texto_janela.insert(tk.END, 'Bem-vindo ao guia para iniciantes no game Terraria!')
+texto_janela.insert(tk.END, 'Terraria é um jogo 2D sandbox extremamente caracteristico e único, tendo o objetivo de derrotar chefões em uma ordem especifica com até quatro classes. Sendo elas:')
+texto_janela.pack()
+
+#Botao classe meelee
+botao_meelee = tk.Button(janela, text='Guerreiro')
+botao_meelee.pack(pady=10)
+
+#Botao classe ranged
+botao_ranged = tk.Button(janela, text='Atirador')
+botao_ranged.pack(pady=10)
+
+#Botao classe mage
+botao_mage = tk.Button(janela, text='Mago')
+botao_mage.pack(pady=10)
+
+#Botao classe sumonner
+botao_sumonner = tk.Button(janela, text='Invocador')
+botao_sumonner.pack(pady=10)
+
+janela.mainloop()
